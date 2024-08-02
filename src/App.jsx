f3edf39f7772db3ae5f260ad756e83ad6102407d
gurmailmaan <90522127+gurmailmaan@users.noreply.github.com>
2024-08-02 02:25:28 -0600

    Ev app 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useState} from "react";
+import Background from "./Components/Background/Background";
 
-export const App = () => {
+const App = () => {
+let heroData = [
+  {text1:"Dive into", text2:"What you love" },
+  {text1:"Indulge", text2:"your passions" },
+  {text1:"Give in to", text2:"your passions"},
+]
+const[heroCount, SetHeroCount] = useState(2);
+const[playStatus,setPlayStatus] = useState(false);
   return (
-    <div>App</div>
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount}/>
+    </div>
   )
 }
 
